a58c3ffd09bcbdc6c8b22e5ef28ae25e982282b9
abeeraikram28 <abeeraikram28@gmail.com>
2021-12-23 23:54:08 +0500

    api service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { SharedModule } from './shared/shared.module'
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -13,6 +13,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     SharedModule,
+    HttpClientModule,
     IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
