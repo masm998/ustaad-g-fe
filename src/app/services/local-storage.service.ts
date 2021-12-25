0974ef4be4dabbbfef2f878d80820c588e16749e
abeeraikram28 <abeeraikram28@gmail.com>
2021-12-25 13:56:47 +0500

    auth, local storage services

--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -6,4 +6,22 @@ import { Injectable } from '@angular/core';
 export class LocalStorageService {
 
   constructor() { }
+
+  saveCredentials(user_id, first_name, last_name, mobile, email, token, profile_pic) {
+    localStorage.setItem("user_id", user_id);
+    localStorage.setItem("mobile", mobile);
+    localStorage.setItem("first_name", first_name);
+    localStorage.setItem("last_name", last_name);
+    localStorage.setItem("email", email);
+    localStorage.setItem("token", token);
+    localStorage.setItem("profile_pic", profile_pic);
+  }
+
+  getUserId() {
+    return localStorage.getItem("user_id")
+  }
+
+  clearLocalStorage() {
+    localStorage.clear()
+  }
 }
