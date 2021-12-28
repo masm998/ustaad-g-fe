cb536e36642cd97c11e5f2ecbef6710b68002f47
abeeraikram28 <abeeraikram28@gmail.com>
2021-12-28 12:42:45 +0500

    ustaad dashboard

---
+++ src/app/modules/ustaad/ustaad-dashboard/ustaad-dashboard.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ustaad-dashboard',
+  templateUrl: './ustaad-dashboard.page.html',
+  styleUrls: ['./ustaad-dashboard.page.scss'],
+})
+export class UstaadDashboardPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/ustaad/ustaad-routing.module.ts
+++ src/app/modules/ustaad/ustaad-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UstaadDashboardPage } from './ustaad-dashboard/ustaad-dashboard.page';
 
 const routes: Routes = [
   {
     path: 'profile',
     loadChildren: ()=> import('./ustaad-profile/ustaad-profile.module').then( m => m.UstaadProfilePageModule)
+  },
+  {
+    path: 'ustaad-dashboard',
+    component: UstaadDashboardPage
   }
+
 ];
 
 @NgModule({
